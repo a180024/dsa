30573a38ee2aa3e6449230ad46f4a71acc5f4dab
a180024 <a180024@e.ntu.edu.sg>
2021-01-19 16:40:47 +0800

    update

--- Data_Structures/hash_table.c
+++ Data_Structures/hash_table.c
@@ -1,164 +1,95 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct set
-{
-    int key;
+#define TABLE_SIZE 10
+
+struct node {
     int data;
+    struct node *next;
 };
 
-struct set *array;
-int capacity = 10;
-int size = 0;
+struct node *head[TABLE_SIZE] = {NULL}, *c;
 
-int hashFunction(int key) {
-    return (key % capacity);
-};
-
-int checkPrime(int n)
-{
-  int i;
-  if (n == 1 || n == 0)
-  {
-    return 0;
-  }
-  for (i = 2; i < n / 2; i++)
-  {
-    if (n % i == 0)
-    {
-      return 0;
-    }
-  }
-  return 1;
-}
-
-int getPrime(int n)
-{
-  if (n % 2 == 0)
-  {
-    n++;
-  }
-  while (!checkPrime(n))
-  {
-    n += 2;
-  }
-  return n;
-}
-
-void init_array() {
-    capacity = getPrime(capacity);
-    array = (struct set *)malloc(capacity * sizeof(struct set));
-    for (int i = 0; i < capacity; i++) {
-        array[i].key = 0;
-        array[i].data = 0;
-    }
-} 
-
-void insert(int key, int data) {
-    int index = hashFunction(key);
-    if (array[index].data == 0) {
-        array[index].key = key;
-        array[index].data = data;
-        size++;
-        printf("\n Key (%d) has been inserted \n", key);
-    }
-    else if (array[index].key == key){
-        array[index].data = data;
-    }
-    else{
-        printf("\n Collision occured  \n");
-    }
-}
-
-void remove_element(int key) {
-    int index = hashFunction(key);
-    if (array[index].key == key) {
-        array[index].key = 0;
-        array[index].data = 0;
-        size--;
-        printf("\n Key (%d) has been removed \n", key);
+void insert(int key) {
+    int i;
+    i = key % TABLE_SIZE;
+    struct node* newNode = (struct node*) malloc(sizeof(struct node));
+    newNode->data = key;
+    newNode->next = NULL;
+    if (head[i] == NULL) {
+        head[i] = newNode;
     } else {
-        printf("\n This key does not exist \n");
+        c = head[i];
+        while (c->next != NULL) {
+            c = c->next;
+        }
+        c->next = newNode;
+    }
+};
+
+void delete(int key) {
+    int i;
+    i = key % TABLE_SIZE;
+    c = head[i];
+    struct node *prev;
+    if (c == NULL) {
+        printf("NO ELEMENT FOUND\n");
+    } else if (c->data == key) {
+        head[i] = c->next;
+    } else {
+        while (c != NULL && c->data != key) {
+            prev = c;
+            c = c->next;
+        }
+        if (c == NULL) {
+           return;
+        }
+        prev->next = c->next;
     }
 }
 
-void display()
-{
-  int i;
-  for (i = 0; i < capacity; i++)
-  {
-    if (array[i].data == 0)
-    {
-      printf("\n array[%d]: / ", i);
+void search(int key) {
+   int i;
+   i = key % TABLE_SIZE;
+   if (head[i] == NULL) {
+       printf("SEARCH ELEMENT NOT FOUND\n");
+   } else {
+       for (c=head[i]; c!=NULL; c=c->next) {
+           if (c->data == key) {
+               printf("SEARCH ELEMENT FOUND\n");
+               break;
+           }
+       }
+       if (c == NULL) {
+           printf("SEARCH ELEMENT NOT FOUND\n");
+       }
+   }
+};
+
+void display() {
+    int i;
+    for (int i=0; i < TABLE_SIZE; i++) {
+        c = head[i];
+        if (c != NULL) {
+            for (c=head[i]; c!=NULL; c=c->next) {
+                printf("%d->",c->data);
+            }
+        }
+        printf("\n");
     }
-    else
-    {
-      printf("\n key: %d array[%d]: %d \t", array[i].key, i, array[i].data);
-    }
-  }
 }
 
-int size_of_hashtable()
-{
-  return size;
+int main() {
+    insert(1);
+    insert(2);
+    insert(11);
+    search(11);
+    display();
+    delete(11);
+    display();
+    delete(1);
+    display();
 }
 
-int main()
-{
-  int choice, key, data, n;
-  int c = 0;
-  init_array();
 
-  do
-  {
-    printf("1.Insert item in the Hash Table"
-         "\n2.Remove item from the Hash Table"
-         "\n3.Check the size of Hash Table"
-         "\n4.Display a Hash Table"
-         "\n\n Please enter your choice: ");
-
-    scanf("%d", &choice);
-    switch (choice)
-    {
-    case 1:
-
-      printf("Enter key -:\t");
-      scanf("%d", &key);
-      printf("Enter data -:\t");
-      scanf("%d", &data);
-      insert(key, data);
-
-      break;
-
-    case 2:
-
-      printf("Enter the key to delete-:");
-      scanf("%d", &key);
-      remove_element(key);
-
-      break;
-
-    case 3:
-
-      n = size_of_hashtable();
-      printf("Size of Hash Table is-:%d\n", n);
-
-      break;
-
-    case 4:
-
-      display();
-
-      break;
-
-    default:
-
-      printf("Invalid Input\n");
-    }
-
-    printf("\nDo you want to continue (press 1 for yes): ");
-    scanf("%d", &c);
-
-  } while (c == 1);
-}
 
--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 2. Stack
 3. Circular Queue
 4. Priority Queue(Max Heap implementation)
-5. Hash Table with Chaining TODO 
+5. Hash Table with Chaining 
 6. Fibonacci Heap
 7. Binary Tree 
 8. Trie(Prefix Tree)
@@ -15,9 +15,9 @@
 2. MergeSort, QuickSort, HeapSort
 3. Binary Search
 4. Graph Traversals(BFS, DFS)
-5. Weighted Graphs(Dijstra, Bellman Ford)  TODO
+5. Weighted Graphs(Dijstra, Bellman Ford) 
 6. Topologiocal Sort TODO
-7. Minimum Spanning Trees(Kruskal's, Prim's) TODO
+7. Minimum Spanning Trees(Kruskal's, Prim's) 
 7. Dynamic Programming(Longest Common Subsequence, Knapsack Problem) TODO
 8. Backtracking TODO
 9. LRU Cache TODO
@@ -25,7 +25,5 @@
 ## Bit Manipulation TODO
 1. Count the number of 1's
 
-## NP TODO
-Travelling salesman
 
 
